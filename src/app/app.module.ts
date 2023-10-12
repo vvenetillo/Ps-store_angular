4f2b41a0746b6a464e96cfb9d59408505b5ecf2c
vvenetillo <venetillovictor@gmail.com>
2023-10-12 16:59:29 -0300

    create update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    MenuBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
