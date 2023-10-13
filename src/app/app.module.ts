0c42ab0487e35859a152ce220760099ceeaa3bbd
vvenetillo <venetillovictor@gmail.com>
2023-10-13 19:59:46 -0300

    update final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPricingComponent } from './components/card/card-pricing/card-pricing.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { FooterComponent } from './components/footer/footer.component';
     HomeComponent,
     CardComponent,
     MenuBarComponent,
-    FooterComponent
+    FooterComponent,
+    CardLabelComponent,
+    CardPricingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent implements OnInit{
+
+  @Input()
+  gameType: string = "DIGITAL PS4"
+
+  @Input()
+  gamePrice: string = "R$399,90"
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+}
